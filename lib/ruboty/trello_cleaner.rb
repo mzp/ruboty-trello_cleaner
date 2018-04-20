2f439bebd225cd7e3e4306a6de72ddc821d310c9
mzp <mzpppp@gmail.com>
2018-04-20 12:28:58 +0900

    :gem: bundle gem ruboty-trello_cleaner

---
+++ lib/ruboty/trello_cleaner.rb
@@ -0,0 +1,7 @@
+require "ruboty/trello_cleaner/version"
+
+module Ruboty
+  module TrelloCleaner
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/trello_cleaner/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module TrelloCleaner
+    VERSION = "0.1.0"
+  end
+end
