096fbd0f5bcc748f8683e95de7365fa22f36f37e
mzp <mzpppp@gmail.com>
2018-04-20 14:41:03 +0900

    move to top of list

--- lib/ruboty/trello_cleaner/actions/cleanup.rb
+++ lib/ruboty/trello_cleaner/actions/cleanup.rb
@@ -21,7 +21,8 @@ module Ruboty
               list.cards.each do |card|
                 if (Time.now - card.created_at) > delay * 3_600
                   card.move_to_list(done_list)
-                  card.update_fields(pos: 'top').update!
+                  card.pos = 1
+                  card.update!
                 end
               end
             end
--- ruboty-trello_cleaner.gemspec
+++ ruboty-trello_cleaner.gemspec
@@ -24,6 +24,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "dotenv"
+  spec.add_dependency 'activemodel', '4.2.10'
   spec.add_dependency "ruboty"
   spec.add_dependency "ruby-trello"
 end
